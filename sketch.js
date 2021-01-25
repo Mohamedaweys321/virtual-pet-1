edaf4f88b756bbd89eb520c40f48ad480d700303
Mohamedaweys321 <71419130+Mohamedaweys321@users.noreply.github.com>
2021-01-24 17:14:20 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var happydog ,dog;
+var database;
+var foodS
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
